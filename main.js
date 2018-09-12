52b082a7488faa875c7d2146191085875ee7aa33
NAME <EMAIL>
2018-09-11 20:49:26 -0400

    got event basic layout and events for code input/output working

---
+++ main.js
@@ -0,0 +1,20 @@
+let elCodeInput = document.getElementById('code-input');
+let elCodeOutput = document.getElementById('display-output');
+let elRunCode = document.getElementById('run-code');
+
+let value = '';
+
+let handleChange = (e) => {
+    value = e.target.value;
+    console.log('event', e);
+};
+
+let handleClick = () => {
+    elCodeOutput.innerHTML = value;
+};
+
+elCodeInput.addEventListener('change', handleChange);
+elRunCode.addEventListener('click', handleClick);
+
+console.log(elCodeOutput);
+console.log(elCodeInput);
