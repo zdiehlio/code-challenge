7521f872abe0bf9e980e7e69ddbc8f35bee2b61a
NAME <EMAIL>
2018-11-09 10:44:38 -0500

    bad code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let elCodeInput = document.getElementById('code-input');
 let elCodeOutput = document.getElementById('display-output');
-let elRunCode = document.getElementById('run-code');
+let elRunBadCode = document.getElementById('run-code');
 
 let value = '';
 
